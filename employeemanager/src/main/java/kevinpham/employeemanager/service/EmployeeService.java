19ce53c7792ca65c8f77c390b51bcfe0145c5f31
Kevin Pham <phambamkevin@gmail.com>
2021-08-31 11:42:23 -0700

    Exposing the API

--- employeemanager/src/main/java/kevinpham/employeemanager/service/EmployeeService.java
+++ employeemanager/src/main/java/kevinpham/employeemanager/service/EmployeeService.java
@@ -37,7 +37,7 @@ public class EmployeeService {
     }
 
 
-    public Employee findEmployeeByIf(Long id) {
+    public Employee findEmployeeById(Long id) {
         return employeeRepo.findEmployeeById(id).orElseThrow(() -> new UserNotFoundException("User by id " + id + " was not found"));
     }
 
