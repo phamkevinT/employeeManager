e026fac15f2bf83677e0637a3eb45ba9d3846be1
Kevin Pham <phambamkevin@gmail.com>
2021-09-01 13:00:41 -0700

    UI Modal functionality

--- frontend/employeemanagerapp/src/app/app.component.ts
+++ frontend/employeemanagerapp/src/app/app.component.ts
@@ -8,7 +8,7 @@ import { EmployeeService } from './employee.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   public employees: Employee[];
 
   constructor(private employeeService: EmployeeService) { }
@@ -26,6 +26,27 @@ export class AppComponent implements OnInit{
         alert(error.message);
       }
     );
+  }
+
+  public onOpenModal(employee: Employee, mode: string): void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if(mode === 'add') {
+      button.setAttribute('data-target', '#addEmployeeModal');
+    }
+    if(mode === 'edit') {
+      button.setAttribute('data-target', '#updateEmployeeModal');
+    }
+    if(mode === 'delete') {
+      button.setAttribute('data-target', '#deleteEmployeeModal');
+    }
+
+    container.appendChild(button);
+    button.click();
+
   }
 
 }
